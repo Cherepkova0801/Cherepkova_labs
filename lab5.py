f4d28b0f7f6e8d37ac52299922dc595d1160e741
Cherepkova0801 <151786914+Cherepkova0801@users.noreply.github.com>
2023-12-06 23:08:09 +0200

    Update lab5.py

--- lab5.py
+++ lab5.py
@@ -1,104 +1,83 @@
 class BankAccount:
-    def __init__(self, account_id, owner_name, balance=0.0):
-        self.__account_id = account_id
+    def __init__(self, id_name, owner_name, balance):
+        self.__id = id_name
         self.__owner_name = owner_name
         self.__balance = balance
 
-    @property
-    def account_id(self):
-        return self.__account_id
+    def get_id(self):
+        return self.__id
     
-    @account_id.setter
-    def account_id(self, a):
-        self.__account_id = a
-
-   
-    @property
-    def owner_name(self):
+    def get_owner_name(self):
         return self.__owner_name
 
-    @owner_name.setter
-    def owner_name(self, name):
-        self.__owner_name = name
-        
-    @property
-    def balance(self):
+    def get_balance(self):
         return self.__balance
-            
-    @balance.setter
-    def balance(self, b):
-        self.__balance = b
-    def deposit(self, amount):
-        self.__balance += amount
-        print(f"You have successfully topped up your balance by {ammount}")
 
-    def withdraw(self, ammount):
+    def deposite(self, amount):
+        self.__balance += amount
+        print("Ви поповнили рахунок на",amount)
+        
+    def withdraw(self, amount):
         if amount <= self.__balance:
             self.__balance -= amount
-            print(f"You have successfully withdrawn {ammount} \nYour balance: {self.__balance}")
+            print("Ви успішно зняли", amount)
+
         else:
-            print("Insufficient funds.")
+            print("Недостатньо коштів")
+            
     def show_account(self):
-        print(f"Account ID: {self.__account_id}, Owner: {self.__owner_name}, Balance: ${self.__balance}")
+        print(f"Account ID:{self.__id}, Owner:{self.__owner_name}, Balance: ${self.__balance}")
+
 class Bank:
     def __init__(self):
         self.__accounts = []
-    @property
-    def accounts(self):
-        return self.__accounts
+    
+    def get_accounts(self):
+        return self.accounts
     
     def add_account(self, account):
-        self.__accounts.append(account)
+        self.accounts.append(account)
    
     def remove_account(self, account_id):
-        for account in self.__accounts:
-            if account.getAccountId() == account_id:
-                self.__accounts.remove(account)
+        for account in self.accounts:
+            if account.get_id() == account_id:
+                self.accounts.remove(account)
                 print(f"Account with ID {account_id} removed.")
                 return
         print(f"Account with ID {account_id} not found.")
-        return False
 
     def sort_accounts_by_balance(self):
-        self.__accounts.sort(key=lambda x: x.getBalance())
+        self.accounts.sort(key=lambda x: x.get_balance())
 
     def show_all_accounts(self):
-        for account in self.__accounts:
+        for account in self.accounts:
             account.show_account()
 
 def main():
-    account1 = BankAccount(1, "John Doe", 1000.0)
-    account2 = BankAccount(2, "Jane Doe", 2000.0)
-    account3 = BankAccount(3, "Bob Smith", 1500.0)
+    account_John_Doe = BankAccount(1, "John Doe", 1000.0)
+    account_Jane_Doe = BankAccount(2, "Jane Doe", 2000.0)
+    account_Bob_Smith = BankAccount(3, "Bob Smith", 1500.0)
 
     bank = Bank()
-    bank.add_account(account1)
-    bank.add_account(account2)
-    bank.add_account(account3)
+    bank.add_account(account_John_Doe)
+    bank.add_account(account_Jane_Doe)
+    bank.add_account(account_Bob_Smith)
 
     print()
     print()
     print("Accounts before sorting:")
     bank.show_all_accounts()
 
-    print()
-    account1.deposit(500.0)
-    account2.deposit(1000.0)
+    print("Accounts before sorting:")
+    bank.show_all_accounts()
+    accounts_John_Doe.deposite(500.0)
+    account_Jane_Doe.withdraw(1000.0)
 
     bank.sort_accounts_by_balance()
 
     print("\nAccounts after sorting:")
     bank.show_all_accounts()
-
-    print()
-    account1.deposit(500.0)
-    account2.deposit(1000.0)
-
-    bank.sort_accounts_by_balance()
-
-    print("\nAccounts after sorting:")
-    bank.show_all_accounts()
-
+    
     bank.remove_account(2)
 
     print("\nAccounts after removing account with ID 2:")
